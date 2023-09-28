ef0c80f3003cb5b63bb8e0eb043f999f3e5ac3d2
Christopher Saucedo <slapdaneck2003@gmail.com>
2023-09-28 17:26:43 -0500

    Updated Health and Damage
    Added a file called attribute manager which we can change later to add more things. Had healthbar linked to health.

---
+++ VenessaDefense/Assets/scripts/Game/AtttributesManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtttributesManager : MonoBehaviour
+{
+    public int health;
+    public int maxHealth = 100;
+    public int attackDamage;
+
+    public HealthBar healthBar;
+
+
+    private void Start()
+    {
+        health = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void takeDamage(int amount)
+    {
+        health -= amount;
+
+        healthBar.SetHealth(health);
+    }
+
+    public void dealDamage(GameObject target)
+    {
+        var atm = target.GetComponent<AtttributesManager>();
+
+        if(atm != null)
+        {
+            atm.takeDamage(attackDamage);
+        }
+    }
+}
