a15f6a71bdf7d2af59a8ae03e376dc4e68888793
Brian Lowry <lowry.brian@att.net>
2023-10-24 17:00:41 -0500

    Shop Toggle

--- VenessaDefense/Assets/Menu.cs
+++ VenessaDefense/Assets/Menu.cs
@@ -7,7 +7,15 @@ public class Menu : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] TextMeshProUGUI currencyUI;
+    [SerializeField] Animator anim;
 
+    private bool isMenuOpen = true;
+
+    public void ToggleMenu()
+    {
+        isMenuOpen = !isMenuOpen;
+        anim.SetBool("ShopOpen", isMenuOpen);
+    }
 
     private void OnGUI()
     {
