a2bedbe6de6993b6646ccbea732519f656548f65
pdsatter <87093940+pdsatter@users.noreply.github.com>
2023-10-24 16:58:07 -0500

    update objectives to be harder

--- VenessaDefense/Assets/scripts/Game/Objectives/ObjectivesManager.cs
+++ VenessaDefense/Assets/scripts/Game/Objectives/ObjectivesManager.cs
@@ -19,9 +19,9 @@ public class ObjectivesManager : MonoBehaviour
     private int numberOfHives = 5;
 
     // Objective Goals
-    private int Level1EnemiesKilledGoal = 1;
+    private int Level1EnemiesKilledGoal = 50;
     private int Level1HivesProtectedGoal = 3;
-    private float Level1HivesProtectedGoalTime = 10.0f;
+    private float Level1HivesProtectedGoalTime = 120.0f;
 
     private Waves wavesCode = null;
 
