646ba7574582b6b3e40173de5d51ec383ecf7c49
Christopher Saucedo <slapdaneck2003@gmail.com>
2023-10-28 19:12:39 -0500

    Added a lot to tutorial
    njoifnbeojfebe

--- VenessaDefense/Assets/scripts/Game/TutorialManager.cs
+++ VenessaDefense/Assets/scripts/Game/TutorialManager.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Windows;
 
 public class TutorialManager : MonoBehaviour
 {
 
     public GameObject[] popUps;
+    public GameObject welcome;
+    //public static event Action closeButton;
     private int popUpIndex;
+    
+
+    public GameObject enemy;
+    /// <summary>
+    /// Using this as condition for the enemy pop up.... Couldnt get it to work tho
+    /// </summary>
 
 
 
@@ -14,15 +26,20 @@ public class TutorialManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        welcome.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void runPopUps()
+    {
         for (int i = 0; i < popUps.Length; i++)
         {
-            if(i == popUpIndex)
+            if (i == popUpIndex)
             {
                 popUps[popUpIndex].SetActive(true);
             }
@@ -32,15 +49,35 @@ public class TutorialManager : MonoBehaviour
             }
         }
 
-        if(popUpIndex == 0)
+        if (popUpIndex == 0) // Movement Pop Up
         {
             if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
                 popUpIndex++;
 
         }
-        else if (popUpIndex == 1)
+        else if (popUpIndex == 1) // Shooting Pop Up
         {
-            if(Input.GetKeyDown(KeyCode.Mouse0))
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+                popUpIndex++;
+        }
+        else if (popUpIndex == 2) // Enemy
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                popUpIndex++;
+        }
+        else if (popUpIndex == 3) // Currency
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                popUpIndex++;
+        }
+        else if (popUpIndex == 4) // Shop
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                popUpIndex++;
+        }
+        else if (popUpIndex == 5) // Skills
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
                 popUpIndex++;
         }
     }
