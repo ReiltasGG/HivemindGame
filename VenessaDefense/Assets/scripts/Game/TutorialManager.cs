540d3d5fec9d7e9b6624dc227336e877ee73b71f
Christopher Saucedo <slapdaneck2003@gmail.com>
2023-10-30 20:52:46 -0500

    Added tutorial stuff

--- VenessaDefense/Assets/scripts/Game/TutorialManager.cs
+++ VenessaDefense/Assets/scripts/Game/TutorialManager.cs
@@ -8,77 +8,112 @@ using System.Windows;
 
 public class TutorialManager : MonoBehaviour
 {
-
+    
     public GameObject[] popUps;
-    public GameObject welcome;
+    //public GameObject welcome;
     //public static event Action closeButton;
     private int popUpIndex;
     
 
-    public GameObject enemy;
+    public GameObject ant;
+    
     /// <summary>
     /// Using this as condition for the enemy pop up.... Couldnt get it to work tho
     /// </summary>
 
 
 
-
+    
     // Start is called before the first frame update
     void Start()
     {
-        welcome.SetActive(true);
+        //runPopUps();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame and studd
     void Update()
     {
         
-    }
-
-    public void runPopUps()
-    {
-        for (int i = 0; i < popUps.Length; i++)
+        if (popUpIndex == 0) // weclcome
         {
-            if (i == popUpIndex)
-            {
-                popUps[popUpIndex].SetActive(true);
-            }
-            else
+            popUps[popUpIndex].SetActive(true);
+            
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 popUps[popUpIndex].SetActive(false);
+                popUpIndex++;
+                popUps[popUpIndex].SetActive(true);
+            }
+            
+            
+
+        }
+        /*
+        else if (popUpIndex == 1) // move
+        {
+            popUps[popUpIndex].SetActive(true);
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                //Debug.Log("IF condition met");
+                popUps[popUpIndex].SetActive(false);
+                popUpIndex++;
+                popUps[popUpIndex].SetActive(true);
             }
         }
-
-        if (popUpIndex == 0) // Movement Pop Up
+        else if (popUpIndex == 2) // shoot
         {
-            if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
-                popUpIndex++;
-
-        }
-        else if (popUpIndex == 1) // Shooting Pop Up
-        {
+            popUps[popUpIndex].SetActive(true);
             if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                popUps[popUpIndex].SetActive(false);
                 popUpIndex++;
+                popUps[popUpIndex].SetActive(true);
+            }
         }
-        else if (popUpIndex == 2) // Enemy
+        else if (popUpIndex == 3) // enemy
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+
+            popUps[popUpIndex].SetActive(true);
+            //ant.SetActive(true);
+            if (GameObject.FindWithTag("Enemy") == null)
+            {
+                popUps[popUpIndex].SetActive(false);
                 popUpIndex++;
+                popUps[popUpIndex].SetActive(true);
+            }
         }
-        else if (popUpIndex == 3) // Currency
+        else if (popUpIndex == 4) // currency
         {
+            popUps[popUpIndex].SetActive(true);
             if (Input.GetKeyDown(KeyCode.Space))
+            {
+                popUps[popUpIndex].SetActive(false);
                 popUpIndex++;
+                popUps[popUpIndex].SetActive(true);
+            }
         }
-        else if (popUpIndex == 4) // Shop
+        else if (popUpIndex == 5) // shop
         {
+            popUps[popUpIndex].SetActive(true);
             if (Input.GetKeyDown(KeyCode.Space))
+            {
+                popUps[popUpIndex].SetActive(false);
                 popUpIndex++;
+                popUps[popUpIndex].SetActive(true);
+            }
         }
         else if (popUpIndex == 5) // Skills
         {
+            popUps[popUpIndex].SetActive(true);
             if (Input.GetKeyDown(KeyCode.Space))
+            {
+                popUps[popUpIndex].SetActive(false);
                 popUpIndex++;
+                //popUps[popUpIndex].SetActive(true);
+            }
+
         }
+        */
     }
+    
 }
